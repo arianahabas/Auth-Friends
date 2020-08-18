39a58566ae039a7523536bbede5869e0ab80f0cd
arianahabas <arianahabas@gmail.com>
2020-08-18 14:15:55 -0500

    init commit

---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const LoginForm = () => {
+    return (
+        <div>
+            <form>
+                <input 
+                type='text'
+                placeholder='email'
+                />
+            </form>
+        </div>
+    )
+}
